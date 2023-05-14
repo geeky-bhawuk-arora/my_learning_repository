3c86cc1343ec1ffcc203ba71c6246a493ab1ea2b
Bhawuk Arora <bhawukarora042@gmail.com>
2023-05-14 10:58:17 +0530

    change

--- ( CS )/( Projects )/( Android Development )/Chapter 2 - Java Refresher/Chapter2AndroidCode/chapter2/src/main/java/com/example/chapter2/ClassEx.java
+++
@@ -1,9 +0,0 @@
-package com.example.chapter2;
-
-public class ClassEx {
-    private int a;
-    private int b;
-    public void method1(){
-        System.out.println("I am method 1");
-    }
-}
--- ( CS )/( Projects )/( Django )/bhawuk_1/bhawuk_2/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from bhawuk_2.models import Services
-
-# Register your models here.
-
-admin.site.register(Services)
